c721e5b1b04b7497eb17dabbd57a947d8c5dabc0
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-05 18:41:26 +0545

    clerk(authentication)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <p>Hello Admin Dashboard</p>;
-}
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
+
+const SetupPage = () => {
+  return (
+    <div className="p-8">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetupPage;
