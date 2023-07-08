8d9babbdc91f44dc0cd3e1cdeefde86510c7d880
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-09 03:21:44 +0545

    main-nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,15 @@
 import { UserButton } from '@clerk/nextjs';
 import React from 'react';
-import { MainNav } from './Main-nav';
+import { MainNav } from '@/components/Main-nav';
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div>this will be store switcher</div>
-        <MainNav />
+        <MainNav className="mx-6" />
         <div className="ml-auto space-x-4">
           <UserButton afterSignOutUrl="/" />
-          NM
         </div>
       </div>
     </div>
