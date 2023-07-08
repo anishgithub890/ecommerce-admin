d24b35feac35eef28df7b1552e1159a4b79a1c37
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-08 20:04:16 +0545

    route-not-create-store-error-fix

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,5 +1,5 @@
-import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
 
@@ -7,19 +7,25 @@ export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
+
     const { name } = body;
+
     if (!userId) {
-      return new NextResponse('unauthorized', { status: 401 });
+      return new NextResponse('Unauthorized', { status: 403 });
     }
+
     if (!name) {
       return new NextResponse('Name is required', { status: 400 });
     }
+
     const store = await prismadb.store.create({
       data: {
         name,
         userId,
       },
     });
+
+    return NextResponse.json(store);
   } catch (error) {
     console.log('[STORES_POST]', error);
     return new NextResponse('Internal error', { status: 500 });
