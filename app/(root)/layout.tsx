dc2f8170545211a750646a7218d52080682d62b8
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-09 02:36:51 +0545

    (sol-for-create-data-and-complete-ref-page)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,31 +3,22 @@ import { redirect } from 'next/navigation';
 
 import prismadb from '@/lib/prismadb';
 
-export default async function DashboardLayout({
+export default async function SetupLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { storeId: string };
 }) {
   const { userId } = auth();
   if (!userId) {
     redirect('/sign-in');
   }
-
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId,
       userId,
     },
   });
-  if (!store) {
-    redirect('/');
+  if (store) {
+    redirect(`/${store.id}`);
   }
-  return (
-    <>
-      <div>This will be a Navbar</div>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
