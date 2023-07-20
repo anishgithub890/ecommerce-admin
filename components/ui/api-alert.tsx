c678e73ecc4e0d5cdf9405cfd8b2dfc3e588ec62
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-20 14:57:00 +0545

    categories(routes)-page-and-columns

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -33,7 +33,7 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
   const [buttonIcon, setButtonIcon] = useState(initialIcon);
 
   const onCopy = () => {
-    setButtonIcon(<CheckCheck className="h-5 w-5 transition text-green-600" />);
+    setButtonIcon(<CheckCheck className="h-4 w-4 transition text-green-600" />);
     setTimeout(() => {
       setButtonIcon(initialIcon);
     }, 1000); // 👈 change icon back after 1 second
