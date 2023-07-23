397d4e0436e60f29cac8800a3f5f576308add191
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-23 19:20:24 +0545

    SizeClient-and-import-on-page

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 
 import prismadb from '@/lib/prismadb';
 
-import { BillboardClient } from './components/client';
+import { SizeClient } from './components/client';
 import { SizeColumn } from './components/columns';
 
 const SizesPage = async ({ params }: { params: { storeId: string } }) => {
@@ -25,7 +25,7 @@ const SizesPage = async ({ params }: { params: { storeId: string } }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedSizes} />
+        <SizeClient data={formattedSizes} />
       </div>
     </div>
   );
