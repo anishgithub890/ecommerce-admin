91d303e203ae8f44425637929802256ee39e7965
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-25 18:04:27 +0545

    color-form:[page]

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -2,19 +2,19 @@ import prismadb from '@/lib/prismadb';
 
 import { ColorForm } from './components/color-form';
 
-const SizePage = async ({ params }: { params: { sizeId: string } }) => {
-  const size = await prismadb.size.findUnique({
+const ColorPage = async ({ params }: { params: { colorId: string } }) => {
+  const color = await prismadb.color.findUnique({
     where: {
-      id: params.sizeId,
+      id: params.colorId,
     },
   });
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ColorForm initialData={size} />
+        <ColorForm initialData={color} />
       </div>
     </div>
   );
 };
 
-export default SizePage;
+export default ColorPage;
