3999eb9abba6d5db54439ff4c064139097134945
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-26 17:46:16 +0545

    product-form

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,8 +1,12 @@
 import prismadb from '@/lib/prismadb';
 
-import { BillboardForm } from './components/billboard-form';
+import { ProductForm } from './components/product-form';
 
-const ProductPage = async ({ params }: { params: { productId: string } }) => {
+const ProductPage = async ({
+  params,
+}: {
+  params: { productId: string; storeId: string };
+}) => {
   const product = await prismadb.product.findUnique({
     where: {
       id: params.productId,
@@ -11,10 +15,32 @@ const ProductPage = async ({ params }: { params: { productId: string } }) => {
       images: true,
     },
   });
+
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardForm initialData={product} />
+        <ProductForm
+          categories={categories}
+          colors={colors}
+          sizes={sizes}
+          initialData={product}
+        />
       </div>
     </div>
   );
