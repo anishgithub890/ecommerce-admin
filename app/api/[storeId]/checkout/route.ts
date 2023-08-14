1e6e7ebea94085ad357a8d9f1c3190e3f983e128
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-15 01:36:31 +0545

    stripe:route

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,7 @@
+import Stripe from 'stripe';
+import { NextResponse } from 'next/server';
+
+import { stripe } from '@/lib/stripe';
+import prismadb from '@/lib/prismadb';
+
+const corsHeaders = {};
