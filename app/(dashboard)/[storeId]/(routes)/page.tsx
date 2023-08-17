8275a02c2a0775cb93065c149920eb4003e01422
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-17 18:11:42 +0545

    components/ui:card.tsx

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,15 +1,20 @@
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
 import prismadb from '@/lib/prismadb';
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
-  return <div className="text-2xl text-blue-400">{store?.name}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
